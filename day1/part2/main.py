0068b9bcfa540251ea888f46655a838146aa2f47
YahyaFazlani <yahyafazlanihere@gmail.com>
2021-12-02 19:51:24 +0530

    better print statements

--- day1/part2/main.py
+++ day1/part2/main.py
@@ -9,11 +9,7 @@ for i in range(2000-3):
   window1 = depths[i] + depths[i + 1] + depths[i + 2]
   window2 = depths[i + 1] + depths[i + 2] + depths[i + 3]
 
-  print(f"{i}: {depths[i]}")
-  print(f"{i+1}: {depths[i+1]}")
-  print(f"{i+2}: {depths[i+2]}")
-  print(f"{i+3}: {depths[i+3]}\n")
   if window2 > window1:
     inc_count += 1
 
-print(inc_count)
+print(f"Windows larger than the previous: {inc_count}")
