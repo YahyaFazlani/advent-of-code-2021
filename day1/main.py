02e33b7c2198fd5254f781af15ca882503efbd81
YahyaFazlani <yahyafazlanihere@gmail.com>
2021-12-02 09:27:38 +0530

    python code

---
+++ day1/main.py
@@ -0,0 +1,13 @@
+depths = []
+inc_count = 0
+
+with open("day1/input.txt", "r") as inputf:
+  for line in inputf:
+
+    depths.append(int(line.strip()))
+  
+for i in range(1999):
+  if depths[i+1] > depths[i]:
+    inc_count += 1
+
+print(f"Depths larger than the previous {inc_count}")
